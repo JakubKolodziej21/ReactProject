fdfc39f13df7255e1acd01a18997403c7e50559f
Jakub Kołodziej <jakub.kolodziej28@microsoft.wsei.edu.pl>
2023-10-29 17:33:51 +0100

    HelloWorldInNewBranch

--- CommunityApp/src/App.tsx
+++ CommunityApp/src/App.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Pinterest from './pages/Pinterest/Pinterest';
 
 export default function App() {
   const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/">
+    <Route path="/" element={<Pinterest />}>
     </Route>
   ));
 
---
+++ CommunityApp/src/pages/Pinterest/Pinterest.tsx
@@ -0,0 +1,9 @@
+import "./Pinterest.css"
+
+export default function Pinterest()
+{
+    return(
+        <h1>Here gonna be a something like Pinterest
+    </h1>
+    )
+}
