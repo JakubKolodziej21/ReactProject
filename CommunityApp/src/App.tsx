ac065f0b6aa1c2b13b23d953ebf80831a6d8fe41
Jakub Kołodziej <jakub.kolodziej28@microsoft.wsei.edu.pl>
2023-10-29 16:03:51 +0100

    LayoutBeforeNewBranches

--- CommunityApp/src/App.tsx
+++ CommunityApp/src/App.tsx
@@ -1,7 +1,15 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import './App.css'
 
 export default function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/">
+    </Route>
+  ));
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <RouterProvider router={router} /> 
+    </>
   )
 }
