51067a445295350f080f5f1d977f3238ab8806b8
Jakub Kołodziej <jakub.kolodziej28@microsoft.wsei.edu.pl>
2023-11-11 16:07:57 +0100

    FirstApiConnection

--- CommunityApp/src/pages/Pinterest/Pinterest.tsx
+++ CommunityApp/src/pages/Pinterest/Pinterest.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from "react"
 import "./Pinterest.css"
+
+
+
 
 export default function Pinterest()
 {
+    const [data,setData]= useState([]); 
+
+    useEffect(()=>{ 
+        async function GetData() {
+                   
+            const response= await fetch('https://jsonplaceholder.typicode.com/albums/1/photos');
+            const posts= await response.json();
+
+            setData(posts);
+            
+        }
+        GetData();
+       
+    },[])
+
+    
+    const dataArray= data.map((photo:any)=>{
+        return(
+            <div>
+            <img src={photo.url} />
+            <p>{photo.title}</p>
+            </div>
+        )
+    })
+
     return(
-        <h1>Here gonna be a something like Pinterest
-    </h1>
+        <div>
+            <h1>
+                Here gonna be a something like Pinterest
+            </h1>
+
+            {dataArray}
+
+        </div>
+        
+
+
     )
 }
