052ba718b75c384e7f81e86801363a7c13cabda4
Jakub Kobialka <kobialka.kontakt@gmail.com>
2023-12-08 22:40:48 +0100

    login page

---
+++ CommunityApp/src/pages/Login/Login.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
